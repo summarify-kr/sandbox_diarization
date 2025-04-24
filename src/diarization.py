5d9a7997a5c5b3fa296fbd083abd4f4b5da84a8f
gaeng02 <sondin144@gmail.com>
2025-04-24 23:51:51 +0900

    issue : no sample code

---
+++ src/diarization.py
@@ -0,0 +1,14 @@
+from diart import SpeakerDiarizationConfig, SpeakerDiarization
+
+import os
+
+if (__name__ == "__main__") : 
+
+    path = "../data/sample.wav"
+
+    config = SpeakerDiarizationConfig (
+        duration = ,
+        step = ,
+        latency = ,
+    )
+    
