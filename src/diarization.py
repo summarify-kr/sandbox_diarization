8c9960e5549e2ea26bd6cf38a7edba154a90c29e
gaeng02 <sondin144@gmail.com>
2025-04-25 23:37:38 +0900

    feat : diarization sample code

--- src/diarization.py
+++ src/diarization.py
@@ -1,14 +1,26 @@
-from diart import SpeakerDiarizationConfig, SpeakerDiarization
+from pyannote.audio import Pipeline
+import torch
 
+from dotenv import load_dotenv
 import os
+
+import warnings
+warnings.filterwarnings("ignore", category = SyntaxWarning)
 
 if (__name__ == "__main__") : 
 
-    path = "../data/sample.wav"
+    load_dotenv()
 
-    config = SpeakerDiarizationConfig (
-        duration = ,
-        step = ,
-        latency = ,
-    )
-    
+    api_key = os.getenv("HUGGINGFACE_ACCESS_TOKEN")
+
+    pipeline = Pipeline.from_pretrained(
+        "pyannote/speaker-diarization-3.1",
+        use_auth_token = api_key)
+
+    pipeline.to(torch.device("cuda"))
+
+    path = "../data/sample.wav"
+    diarization = pipeline(path)
+
+    for turn, _, speaker in diarization.itertracks(yield_label = True) :
+        print(f"start={turn.start : .1f}s stop={turn.end : .1f}s speaker_{speaker}")
