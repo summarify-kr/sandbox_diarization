55d50e2c9012795a42749e72c0a94ea815bc08cc
gaeng02 <sondin144@gmail.com>
2025-04-27 23:43:09 +0900

    add : converter m4a to wav

---
+++ src/converter.py
@@ -0,0 +1,15 @@
+from pydub import AudioSegment
+import os
+
+def convert_m4a_to_wav (m4a_path, wav_path) : 
+    
+    audio = AudioSegment.from_file(m4a_path, format = "m4a")
+
+    audio.export(wav_path, format = "wav")
+
+if (__name__ == "__main__") : 
+    m4a_file = ""
+    wav_file = ""
+
+    convert_m4a_to_wav(m4a_file, wav_file)
+    print("Convert Completed")
