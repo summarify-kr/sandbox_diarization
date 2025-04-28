a3c2ce272ab9619c2632f94a5e87072ec918bbd9
gaeng02 <sondin144@gmail.com>
2025-04-28 23:36:47 +0900

    test : extension converter

--- src/converter.py
+++ src/converter.py
@@ -8,8 +8,8 @@ def convert_m4a_to_wav (m4a_path, wav_path) :
     audio.export(wav_path, format = "wav")
 
 if (__name__ == "__main__") : 
-    m4a_file = ""
-    wav_file = ""
+    m4a_file = "../data/sample.m4a"
+    wav_file = "../data/sample.wav"
 
     convert_m4a_to_wav(m4a_file, wav_file)
     print("Convert Completed")
