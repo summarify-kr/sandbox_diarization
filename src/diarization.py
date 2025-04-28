b0f8ac9a6558946e4f1cb8b8a8c5e2a94d435ca3
gaeng02 <sondin144@gmail.com>
2025-04-28 23:41:29 +0900

    chore : hugging face connect

--- src/diarization.py
+++ src/diarization.py
@@ -6,21 +6,26 @@ import os
 
 import warnings
 warnings.filterwarnings("ignore", category = SyntaxWarning)
+warnings.filterwarnings("ignore", category = UserWarning)
 
 if (__name__ == "__main__") : 
 
-    load_dotenv()
+    load_dotenv(dotenv_path="../.env")
 
     api_key = os.getenv("HUGGINGFACE_ACCESS_TOKEN")
+
+    print(api_key)
 
     pipeline = Pipeline.from_pretrained(
         "pyannote/speaker-diarization-3.1",
         use_auth_token = api_key)
 
-    pipeline.to(torch.device("cuda"))
+    '''
+    pipeline.to(torch.device("cpu"))
 
     path = "../data/sample.wav"
     diarization = pipeline(path)
 
     for turn, _, speaker in diarization.itertracks(yield_label = True) :
         print(f"start={turn.start : .1f}s stop={turn.end : .1f}s speaker_{speaker}")
+    '''
